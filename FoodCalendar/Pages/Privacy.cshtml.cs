b1c3538527fc29490b40a238cfa7a208b297ddb4
sophiadrewfs <49346118+sophiadrewfs@users.noreply.github.com>
2023-03-01 12:38:38 -0600

    Hello World

--- FoodCalendar/Pages/Privacy.cshtml.cs
+++ FoodCalendar/Pages/Privacy.cshtml.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
 
 namespace FoodCalendar.Pages
 {
@@ -14,6 +14,8 @@ namespace FoodCalendar.Pages
 
         public void OnGet()
         {
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            ViewData["TimeStamp"] = dateTime;
         }
     }
 }
