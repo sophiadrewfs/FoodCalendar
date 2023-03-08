67f5420fa183fefaaa271bd15f8f4bbd02460f4c
sophiadrewfs <49346118+sophiadrewfs@users.noreply.github.com>
2023-03-08 16:33:54 -0600

    more updates

---
+++ FoodCalendar/Migrations/20230308201232_Create2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodCalendar.Migrations
+{
+    public partial class Create2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Recipe",
+                table: "Food",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Recipe",
+                table: "Food");
+        }
+    }
+}
--- FoodCalendar/Models/Food.cs
+++ FoodCalendar/Models/Food.cs
@@ -7,18 +7,24 @@ namespace FoodCalendar.Models
     public class Food
     {
         public int Id { get; set; }
+        [Display(Name = "Dish")]
         public string? FoodName { get; set; }
+        [Display(Name = "Last Ate")]
         [DataType(DataType.Date)]
         public DateTime LastAte { get; set; }
+        [Display(Name = "Dish Type")]
         public int TypeId { get; set; }
         [ForeignKey("TypeId")]
-        public DishType? DishType { get; set; } 
+        public DishType? DishType { get; set; }
+        [Display(Name = "# of People")]
         public int NumofPeople { get; set; }
+        public string? Recipe { get; set; }
     }
 
     public class DishType
     { 
         public int Id { get; set; }
+        [Display(Name = "Dish Type")]
         public string DishTypeName { get; set; } = string.Empty;
     }
 }
